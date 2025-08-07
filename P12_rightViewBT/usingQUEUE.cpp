5f2bd58ce2afdc11acda8e7e239d7df1a4504c4f
gargbhavya2265 <bhavyagarg2265@gmail.com>
2025-08-07 15:18:16 +0530

    Add files via upload

---
+++ P12_rightViewBT/usingQUEUE.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    //constructor..
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+Node* BinaryTree(){
+    int x;
+    cin>>x;
+    if(x==-1){
+        return NULL;
+    }
+    Node* temp=new Node(x);
+    cout<<"Enter the leftchild of "<<x<<endl;
+    temp->left=BinaryTree();
+    cout<<"Enter the rightchild of "<<x<<endl;
+    temp->right=BinaryTree();
+
+    return temp;
+}
+void righttViewBT(Node* root,vector<int>&result)
+{
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        int n=q.size();
+        result.push_back(q.front()->data);
+        for(int i=0;i<n;i++){
+            Node* temp=q.front();
+            q.pop();
+            if(temp->right) q.push(temp->right);
+            if(temp->left)  q.push(temp->left);
+        }
+    }
+}
+int main(){
+    cout<<"Enter the rootNode"<<endl;
+    Node* root=BinaryTree();
+    vector<int>result;
+    righttViewBT(root,result);
+    cout<<"RightView Of BT is: ";
+    for(int i=0;i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
---
+++ P12_rightViewBT/usingRECURSION.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    //constructor..
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+Node* BinaryTree(){
+    int x;
+    cin>>x;
+    if(x==-1){
+        return NULL;
+    }
+    Node* temp=new Node(x);
+    cout<<"Enter the leftchild of "<<x<<endl;
+    temp->left=BinaryTree();
+    cout<<"Enter the rightchild of "<<x<<endl;
+    temp->right=BinaryTree();
+
+    return temp;
+}
+int heightBT(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    return 1+max(heightBT(root->left),heightBT(root->right));
+}
+void rightViewBT(Node* root,vector<int>&result,vector<int>&levelvisited,int level){
+    if(root==NULL)  return;
+    if(levelvisited[level]==0){
+        result.push_back(root->data);
+        levelvisited[level]=1;
+    }
+    rightViewBT(root->right,result,levelvisited,level+1);
+    rightViewBT(root->left,result,levelvisited,level+1);
+
+}
+int main(){
+    cout<<"Enter the rootNode"<<endl;
+    Node* root=BinaryTree();
+    vector<int>result;
+    int height=heightBT(root);
+    vector<int>levelvisited(height,0);
+    rightViewBT(root,result,levelvisited,0);
+    cout<<"RightView Of BT is: ";
+    for(int i=0;i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
